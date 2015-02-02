d1960107542fc7bf0fe502b5e190e7668958ec4d
Woody Lewis <woody@woodylewis.com>
2015-02-02 15:31:33 -0500

    Combine socket code

--- trade-server.js
+++ trade-server.js
@@ -1,27 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+/*
 app.get('/', function(req, res) {
 	//-- sendFile instead of send -- 
 	res.sendFile(__dirname + '/index.html');
 });
+*/
 
 io.on('connection', function(socket) {
 	console.log('RECEIVED CONNECTION');
-	var theStr = 'TEST SERVER MESSAGE';
-
+	var theStr = 'SERVER RESPONSE';
 	
 	socket.on('client_request', function(msg){
 		io.emit('client_request', msg);
 		console.log('client_request: ' + msg);
+		io.emit('client_request', theStr);
+		console.log('client_request: ' + theStr);
 	});
-/*
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
-		console.log('message: ' + msg);
-	});
-*/
 });
 
 http.listen(3500, function(){
