ddc93b33174692643c8909480c62c363aa00ac62
Woody Lewis <woody@woodylewis.com>
2015-02-11 12:49:55 -0500

    Finish buy logic

--- trade-server.js
+++ trade-server.js
@@ -1,38 +1,29 @@
 var app = require('express')(),
 http = require('http').Server(app),
-io = require('socket.io')(http),
-mongoose = require('mongoose');
+io = require('socket.io')(http);
 
 var Portfolio = require("./app/trade.js");
 
-//---- Replace with real credentials -------
-//mongoose.connect('mongodb://<dbuser>:<dbpassword>@mongodb_instance');
-//---- Local dev instance
-/*
-mongoose.connect('mongodb://localhost/portfolio');
-*/
 io.on('connection', function(socket) {
 	var cashPosition = 1000000;
+	var tranche = 100000;
 	var portfolio = new Portfolio(io, cashPosition);
-	portfolio.init();
+
+	socket.on('init', function(msg){
+		console.log('init', msg);
+		portfolio.init();
+	});
 
 	socket.on('buy', function(msg){
-		console.log('buy', msg);
-		switch (msg) {
-			case 'stock1':
-				portfolio.stock1  = 100000;
-				portfolio.buy1();
-			break;
-			case 'stock2':
-				portfolio.stock2  = 100000;
-				portfolio.buy2();
-			break;
-			case 'stock3':
-				portfolio.stock3  = 100000;
-				portfolio.buy3();
-			break;
+		if(portfolio.cashPosition != 0) {
+			portfolio.cashPosition -= tranche;
+			portfolio.cash();
+			console.log('buy', msg);
 		}
-		portfolio.cash = 900000;
+		else {
+			portfolio.noCash();
+			console.log('no cash');
+		}
 	});
 });
 
